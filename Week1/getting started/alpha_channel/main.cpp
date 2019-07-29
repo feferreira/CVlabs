4820456ae57db4a1e7a96cbdc180b3c392e70d58
Fernando Ferreira Silva <fernando@ferreirasil.com>
2019-07-28 21:30:30 -0300

    getting started

---
+++ Week1/getting started/alpha_channel/main.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main(){
+    string imagePath = "./panther.png";
+    
+    Mat imgPng = imread(imagePath, -1); //le a imagem como ela é
+    cout << "image Size: " << imgPng.size() << "\nimage channel: " << imgPng.channels() << '\n';
+    
+    Mat imgBGR;
+    Mat imgPngChannels[4];
+    split(imgPng, imgPngChannels);
+    
+    merge(imgPngChannels, 3, imgBGR);
+    
+    Mat imgPngMask = imgPngChannels[3];
+    
+    imwrite("colorChannels.png", imgBGR);
+    imwrite("alphaChannel.png", imgPngMask);
+    
+    return 0;
+}
---
+++ Week1/getting started/color_pixel/main.cpp
@@ -0,0 +1,34 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main(){
+    string imagePath = "./number_zero.jpg";
+    Mat testImage = imread(imagePath, 1); //carrega uma imagem pb como colorida
+    
+    //access color pixel
+    cout << testImage.at<Vec3b>(0,0) << '\n';
+    
+    //modify pixel
+    //change 0,0 to yellow
+    // 1,1 to cyan
+    // 2,2 to magenta
+    testImage.at<Vec3b>(0,0) = Vec3b(0,255,255);
+    imwrite("zero1.png", testImage);
+    testImage.at<Vec3b>(1,1) = Vec3b(255,255,0);
+    imwrite("zero2.png", testImage);
+    testImage.at<Vec3b>(2,2) = Vec3b(255,0,255);
+    imwrite("zero3.png", testImage);
+    
+    //modify region
+    testImage(Range(0,3),Range(0,3)).setTo(Scalar(255,0,0));
+    testImage(Range(3,6),Range(0,3)).setTo(Scalar(0,255,0));
+    testImage(Range(6,9),Range(0,3)).setTo(Scalar(0,0,255));
+    imwrite("zeroModified.png", testImage);
+    
+    return 0;
+}
