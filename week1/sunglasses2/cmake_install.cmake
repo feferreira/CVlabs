4877fefc34dc9b1854428d30c634ff76edcb8b86
Fernando Ferreira Silva <fernando@ferreirasil.com>
2021-02-03 22:51:08 -0300

    sunglass2 working

--- week1/sunglasses2/cmake_install.cmake
+++ week1/sunglasses2/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fernando/work/CVlabs/week1/sunglasses
+# Install script for directory: /home/fernando/work/CVlabs/week1/sunglasses2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/fernando/work/CVlabs/week1/sunglasses/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/fernando/work/CVlabs/week1/sunglasses2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- week1/sunglasses2/sunglasses.cpp
+++ week1/sunglasses2/sunglasses.cpp
@@ -8,70 +8,77 @@
 int main(){
     //set path's
     const char * sunglassPath = "../images/sunglasses.png";
-    const char * chicoPath = "../images/chico.jpg";
+    const char * billiePath = "../images/billie.jpg";
     //open image files
     cv::Mat sunglasses = cv::imread(sunglassPath, cv::IMREAD_UNCHANGED);
-    cv::Mat chico = cv::imread(chicoPath);
+    cv::Mat billie = cv::imread(billiePath);
         
     std::cout << "size of sunglasses: " << sunglasses.size() << '\n';
-    std::cout << "size of chico: " << chico.size() << '\n';
+    std::cout << "size of billie: " << billie.size() << '\n';
     std::cout << "channels of sunglasses: " << sunglasses.channels() << '\n';
-    std::cout << "channels of chico: " << chico.channels() << '\n';
+    std::cout << "channels of billie: " << billie.channels() << '\n';
     
     //resize sunglasses to fit in images
     cv::resize(sunglasses, sunglasses, cv::Size(100,80));
     int glassHeight = sunglasses.size().height;
     int glassWidth = sunglasses.size().width;
     
-    //separate channels of sunglasses
+    cv::Mat billieWithGlassBitwise = billie.clone();
+
     cv::Mat glassRgbaChannels[4];
-    cv::split(sunglasses, glassRgbaChannels);
-    
-    //separate glass mask from glassBgr
     cv::Mat glassRgbChannels[3];
-    for(int i = 0; i < 3; i++){
+
+    cv::split(sunglasses, glassRgbaChannels);
+    for(int i = 0; i <3; i++){
         glassRgbChannels[i] = glassRgbaChannels[i];
     }
-    
-    //create a bgr image without alpha channel
-    cv::Mat glassBgr;
+    cv::Mat glassBgr, glassMask1;
     cv::merge(glassRgbChannels,3,glassBgr);
-    cv::Mat glassBgr2 = glassBgr;
-    //create a 3 channel mask with the alphaChannel
-    cv::Mat glassMask1 = glassRgbaChannels[3];
-    cv::Mat glassMaskChannels[3] = {glassMask1, glassMask1, glassMask1};
-    cv::Mat glassMask;
-    cv::merge(glassMaskChannels,3,glassMask);
-    cv::Mat glassMask2 = glassMask.clone(); 
-    //change range from 0 to 1 
-    glassMask = glassMask/255;
+    glassMask1 = glassRgbaChannels[3];
 
-    //create a clone of image and get the eye region
-    cv::Mat chicoGlasses = chico.clone();
-    int startEyeR = 180;
+    int startEyeR = 160;
     int startEyeC = 85;
-    cv::Mat chicoEyeRoi = chicoGlasses(cv::Range(startEyeR,startEyeR+glassHeight),
+    cv::Mat eyeRoi = billieWithGlassBitwise(cv::Range(startEyeR,startEyeR+glassHeight),
             cv::Range(startEyeC,startEyeC+glassWidth));
-    cv::Mat chicoEyeRoiChannels[3];
-    cv::split(chicoEyeRoi,chicoEyeRoiChannels);
-    
-    cv::Mat maskedEyeChicoChannels[3];
-    cv::Mat maskedEyeChico;
-
-    for (int i = 0; i < 3; i++)
-    {
-         multiply(chicoEyeRoiChannels[i], (1-glassMaskChannels[i]), maskedEyeChicoChannels[i]);
+    cv::Mat glassMask;
+    cv::Mat glassMaskChannels[]={glassMask1, glassMask1, glassMask1};
+    cv::merge(glassMaskChannels,3,glassMask);
+    cv::Mat glassMaskNot;
+    cv::bitwise_not(glassMask1, glassMaskNot);
+    cv::Mat eye;
+    cv::Mat eyeChannels[3];
+    cv::Mat eyeRoiChannels[3];
+    cv::Mat maskedGlass;
+    cv::Mat eyeRoifinal;
+    cv::split(eyeRoi, eyeRoiChannels);
+    for(int i=0; i<3; i++){
+        cv::bitwise_and(eyeRoiChannels[i],glassMaskNot,eyeChannels[i]);
     }
-    cv::merge(maskedEyeChicoChannels,3,maskedEyeChico);
     
-    cv::Mat maskedChicoGlass;
-    multiply(glassBgr,glassMask,maskedChicoGlass);
+    std::cout << "cheguei\n";    
+    cv::Mat glassMaskNotChannels[]={glassMaskNot, glassMaskNot, glassMaskNot};
+    cv::Mat glassNotMerged;
+    cv::merge(glassMaskNotChannels,3,glassNotMerged);
 
-    cv::Mat eyeChicoRoiFinal;
-    cv::add(maskedEyeChico,maskedChicoGlass,eyeChicoRoiFinal);
+    cv::bitwise_and(eyeRoi, glassNotMerged, eye);
+
+    cv::Mat sunglassRegion;
+    cv::Mat sunglassRegionChannels[3];
+    cv::Mat glassRegionBgrChannels[3];
+
+    split(glassBgr, glassRegionBgrChannels);
+    for(int i=0; i<3; i++){
+        cv::bitwise_and(glassRegionBgrChannels[i], glassMask1, sunglassRegionChannels[i]);
+    }
     
-    eyeChicoRoiFinal.copyTo(chicoEyeRoi);
-    cv::imshow("chico", chicoGlasses);
+    cv::merge(sunglassRegionChannels,3,sunglassRegion);
+
+    cv::multiply(glassBgr, glassMask, maskedGlass);
+
+    cv::bitwise_or(eye,sunglassRegion,eyeRoifinal);
+    eyeRoifinal.copyTo(eyeRoi);
+
+    cv::imshow("billie", billieWithGlassBitwise);
     
     cv::waitKey(0);
     return 0;
